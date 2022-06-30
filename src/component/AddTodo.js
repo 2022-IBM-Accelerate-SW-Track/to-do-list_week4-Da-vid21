ecac01f2d8eb032f04f78c998ef5764c429b977f
David Tekeste <tekestede@vcu.edu>
2022-06-30 14:23:26 -0400

    Test

--- src/component/AddTodo.js
+++ src/component/AddTodo.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { Button, TextField } from "@mui/material";
 import { DesktopDatePicker , LocalizationProvider} from '@mui/x-date-pickers';
 import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
+import Axios from "axios";
 
 class AddTodo extends Component {
   // Create a local react state of the this component with both content date property set to nothing.
@@ -39,6 +40,22 @@ class AddTodo extends Component {
   handleSubmit = (event) => {
     event.preventDefault();
     if (this.state.content.trim()) {
+      const jsonObject = {
+        id: this.state.id,
+        task: this.state.content,
+        currentDate: this.state.date,
+        dueDate: this.state.duedate,
+      };
+      Axios({
+        method: "POST",
+        url: "http://localhost:8000/add/item",
+        data: {jsonObject},
+        headers: {
+           "Content-Type": "application/json"
+        }
+     }).then(res => {
+        console.log(res.data.message);
+     });
       this.props.addTodo(this.state);
       this.setState({
         content: "",
@@ -49,13 +66,11 @@ class AddTodo extends Component {
   };
   render() {
     return (
-      // 1. When rendering a component, you can render as many elements as you like as long as it is wrapped inside
-      // one div element.
-      // 2. The return statement should include a text field input with the handleChange function from above that
-      // is passed into an onChange event.
-      // 3. The return should also include a button with the handleSubmit function from above that is passed into
-      // an OnClick event.
-      // 4. The value of the text field also should reflect the local state of this component.
+      /** 1. When rendering a component, you can render as many elements as you like as long as it is wrapped inside one div element.
+       2. The return statement should include a text field input with the handleChange function from above that is passed into an onChange event.
+       3. The return should also include a button with the handleSubmit function from above that is passed into an OnClick event.
+       4. The value of the text field also should reflect the local state of this component.
+       */
       <div>
         <TextField
           label="Add New Item"
